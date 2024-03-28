1e2e0c28632c182fdf6fd0d893b3c2a0c7bd83a1
眼 万 年 <135793580+eryknguyen-infjt@users.noreply.github.com>
2024-03-28 21:51:10 +0700

    command lines

--- st.js
+++ st.js
@@ -1,9 +1,22 @@
 const sequence = 'Hello World'
 
 let numberInt 
-numberInt = 240710
-numberInt = 'NTTPNHG'
+numberInt = 240710 // this is number data type
+numberInt = 'NTTPNHG' // this is string data type
 
+// null (data type) is the absense of the val.
+// another is undefined (equals to nothing)
+
+true && false // these are booleans data type
+
+let nameBooks = {
+    Achilles: 'should read',
+    Leonardo_daVinci: 'great exercises for brain',
+    Einstein: sequence
+}
 
 console.log(sequence)
-console.log(numberInt)
+console.log(nameBooks)
+
+// These are two slashes structure for comment in JS
+
