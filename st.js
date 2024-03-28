31d14c6d0be502308e1dec371dd50f93ef99250d
眼 万 年 <135793580+eryknguyen-infjt@users.noreply.github.com>
2024-03-28 22:11:46 +0700

    command lines

--- st.js
+++ st.js
@@ -9,14 +9,31 @@ numberInt = 'NTTPNHG' // this is string data type
 
 true && false // these are booleans data type
 
-let nameBooks = {
+let dictionary_objects = {
     Achilles: 'should read',
     Leonardo_daVinci: 'great exercises for brain',
     Einstein: sequence
-}
+} //objects or dictionary
+
+let array = [1, 2, 5, 6, 7, 9, 2, 0, 3490]
 
 console.log(sequence)
-console.log(nameBooks)
+console.log(dictionary_objects)
 
 // These are two slashes structure for comment in JS
 
+
+// functions
+
+function fstFunctions() {
+    console.log(console)
+}
+
+fstFunctions()
+
+function sndFunction(val, value) {
+    console.log(val * value)
+}
+
+sndFunction(4, 5)
+sndFunction(6, 7)
